64190ccfa7f9ce5a9b1616fec66d4f73109796f5
t3chvest <LKEnkhoYw43j#bQRicQzmA$Maq>
2020-09-16 00:41:45 -0400

    reupload

--- cc_info/lib/screens/about.dart
+++ cc_info/lib/screens/about.dart
@@ -6,7 +6,28 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Things stuff n whatnot'),
+      child: Center(
+        child: Column(
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              "assets/icons/steve.jpg",
+              width: 275,
+              //scale: 3,
+              //fit: BoxFit.fitHeight,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  "Application written by Steve Moore\n"
+                  "Scientific data from SkepticalScience.com\n\n"
+                  "Steve is a graduate of GWU's class of 2020, so long as this app doesn't cause him to fail his capstone",
+                  textAlign: TextAlign.center),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
